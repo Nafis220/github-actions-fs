b8f4c976efac11a1cd6312da6dfbff7c1e996c27
bob <bob@>
2024-05-27 17:58:57 +0600

    second commit

--- index.mjs
+++ index.mjs
@@ -2,7 +2,7 @@ import express from "express";
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello World");
+  res.send("Hello Github Actions");
 });
 app.listen(3000, () => {
   console.log("Listening in 3000");
